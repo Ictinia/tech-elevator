3dc02a362e77fca943516817cf6306c1292e35f0
jason heaton <jasonmheaton@gmail.com>
2023-04-12 17:10:13 -0400

    LandmarkCardPageAdded

---
+++ vue/src/components/LandmarkCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div
+      class="
+        grid grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        gap-4
+      "
+    >
+      <div v-for="(item, index) in items" :key="index">
+        <picture-description
+          :imageUrl="item.imageUrl"
+          :imageAlt="item.imageAlt"
+          :description="item.description"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
