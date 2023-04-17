f5980ab3e01ba48f99dda6234c05e912d7b4d40c
David Stehura <david.stehura@gmail.com>
2023-04-17 15:39:16 -0400

    addReview API tested and working

--- java/src/main/java/com/techelevator/controller/ReviewController.java
+++ java/src/main/java/com/techelevator/controller/ReviewController.java
@@ -2,25 +2,30 @@ package com.techelevator.controller;
 
 
 import com.techelevator.dao.ReviewDao;
+import com.techelevator.dao.UserDao;
 import com.techelevator.model.ItineraryDto;
 import com.techelevator.model.LandmarkDto;
 import com.techelevator.model.Review;
 import com.techelevator.model.ReviewDto;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 
+import java.security.Principal;
 import java.util.List;
 
 @CrossOrigin
 @RestController
 public class ReviewController {
     private ReviewDao reviewDao;
+    private final UserDao userDao;
 
-    public ReviewController(ReviewDao dao) {
+    public ReviewController(ReviewDao dao, UserDao userDao) {
         this.reviewDao = dao;
+        this.userDao = userDao;
     }
 
     @GetMapping(path = "/landmarks/{id}/reviews")
@@ -28,17 +33,12 @@ public class ReviewController {
         return reviewDao.getReviewsByLandmarkId(id);
     }
 
+    @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "/landmarks/{id}/reviews")
-    public Review addReview(@PathVariable int id, @RequestBody ReviewDto reviewDto) {
-        Review review = new Review(
-                0,
-                id,
-                reviewDto.getUserId(),
-                reviewDto.getTitle(),
-                reviewDto.getDescription()
-        );
-        return reviewDao.addReview(review);
+    public void addReview(@PathVariable int id, @RequestBody ReviewDto reviewDto, Principal principal) {
+        final int userId = userDao.findIdByUsername(principal.getName());
+        reviewDao.addReview(id, reviewDto, userId);
     }
 }
 
--- java/src/main/java/com/techelevator/model/ReviewDto.java
+++ java/src/main/java/com/techelevator/model/ReviewDto.java
@@ -1,36 +1,10 @@
 package com.techelevator.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ReviewDto {
-    private int reviewId;
-    private int landmarkId;
-    private int userId;
     private String title;
     private String description;
-
-
-    public int getReviewId() {
-        return reviewId;
-    }
-
-    public void setReviewId(int reviewId) {
-        this.reviewId = reviewId;
-    }
-
-    public int getLandmarkId() {
-        return landmarkId;
-    }
-
-    public void setLandmarkId(int landmarkId) {
-        this.landmarkId = landmarkId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
 
     public String getTitle() {
         return title;
@@ -47,17 +21,5 @@ public class ReviewDto {
     public void setDescription(String description) {
         this.description = description;
     }
-
- /*   @Override
-    public String toString() {
-        return "ReviewDto{" +
-                "reviewid=" + reviewId +
-                "landmarkid=" + landmarkId +
-                ", userId=" + userId +
-                ", name='" + title + '\'' +
-                ", date=" + description +
-                '}';*/
-
-
-    }
+}
 
