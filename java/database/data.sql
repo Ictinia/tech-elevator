95c8eaa372fb7924f0a5e5b23a68a6412e66818f
David Stehura <david.stehura@gmail.com>
2023-04-18 15:28:50 -0400

    foo

--- java/database/data.sql
+++ java/database/data.sql
@@ -258,18 +258,18 @@ INSERT INTO itineraries (user_id, name, date)
 VALUES (1, 'test', '2023-05-05');
 
 INSERT INTO itinerary_details (itin_id, landmark_id, sequence_number) VALUES 
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4),
-(1, 5),
-(1, 6),
-(1, 7),
-(1, 8),
-(1, 9),
-(1, 10),
-(1, 11),
-(1, 12),
+(1, 1, 12),
+(1, 2, 11),
+(1, 3, 10),
+(1, 4, 1),
+(1, 5, 2),
+(1, 6, 3),
+(1, 7, 4),
+(1, 8, 5),
+(1, 9, 6),
+(1, 10, 7),
+(1, 11, 8),
+(1, 12, 9);
 
 
 COMMIT TRANSACTION;
