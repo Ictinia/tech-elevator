3275c11cde494976a8100922bf41c37ac72c444f
David Stehura <david.stehura@gmail.com>
2023-04-18 10:23:55 -0400

    Added dummy itinerary with dummy stops

--- java/database/data.sql
+++ java/database/data.sql
@@ -257,4 +257,22 @@ INSERT INTO reviews (landmark_id, user_id, title, description) VALUES ((SELECT l
 INSERT INTO itineraries (user_id, name, date)
 VALUES (1, 'test', '2023-05-05');
 
+INSERT INTO itinerary_details (itin_id, landmark_id, sequence_number) VALUES 
+(1, 1, nextval('itinerary_sequence')),
+(1, 2, nextval('itinerary_sequence')),
+(1, 3, nextval('itinerary_sequence')),
+(1, 4, nextval('itinerary_sequence')),
+(1, 5, nextval('itinerary_sequence')),
+(1, 6, nextval('itinerary_sequence')),
+(1, 7, nextval('itinerary_sequence')),
+(1, 8, nextval('itinerary_sequence')),
+(1, 9, nextval('itinerary_sequence')),
+(1, 10, nextval('itinerary_sequence')),
+(1, 11, nextval('itinerary_sequence')),
+(1, 12, nextval('itinerary_sequence')),
+(1, 13, nextval('itinerary_sequence')),
+(1, 14, nextval('itinerary_sequence')),
+(1, 15, nextval('itinerary_sequence')),
+(1, 16, nextval('itinerary_sequence'));
+
 COMMIT TRANSACTION;
